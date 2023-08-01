7dc46de7d9b422ebbf2c08ac71d4a5c074314c75
kahles17 <simon.roehrl01@gmail.com>
2023-08-02 00:35:29 +0200

    Add Code

---
+++ src/index.php
@@ -0,0 +1,11 @@
+<?php 
+
+// Insert your PHP Code here
+
+// Parse the csv file
+
+// Create the html skeleton and integrate the style.css file
+
+// Loop over the rows in the csv file to generate the right content for the cards
+
+// Output the HTML Code for the cards
